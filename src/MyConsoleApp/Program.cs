925ec04cd7d50277f88e6419af5694e594d47e46
huhubun <nongzc@outlook.com>
2020-10-28 03:05:24 +0800

    feat: 控制台输出内容修改

--- src/MyConsoleApp/Program.cs
+++ src/MyConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace MyConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 666!");
         }
     }
 }
