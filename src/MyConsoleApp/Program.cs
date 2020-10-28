cb49fc187ea855e102f588240c6055d049a0c9dd
huhubun <nongzc@outlook.com>
2020-10-28 17:17:20 +0800

    feat: 每天改为 DateTime.Now 动态生成

--- src/MyConsoleApp/Program.cs
+++ src/MyConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World 996!");
-            Console.WriteLine("每天 007!");
+            Console.WriteLine($"{DateTime.Now} 007!");
         }
     }
 }
