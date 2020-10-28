63988d5365bd6f73fb02ea2b4eba40a34b941a92
huhubun <nongzc@outlook.com>
2020-10-28 11:01:24 +0800

    feat: 移除输出 `hehehe` 的功能

--- src/MyConsoleApp/Program.cs
+++ src/MyConsoleApp/Program.cs
@@ -7,7 +7,6 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World 666!");
-            Console.WriteLine("hehehe");
         }
     }
 }
