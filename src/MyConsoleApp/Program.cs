e9a0ee6c689cd3ef319988c4c4fef5b7f7765de5
huhubun <nongzc@outlook.com>
2020-10-28 11:04:17 +0800

    feat: 增加输出内容 `每天 007`

--- src/MyConsoleApp/Program.cs
+++ src/MyConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World 996!");
+            Console.WriteLine("每天 007!");
         }
     }
 }
